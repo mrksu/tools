f4a4f8f5ccd473af837b07f5274a948495206b7e
Marek Suchánek <msuchane@redhat.com>
2019-02-24 01:00:48 +0100

    Provide a DocBook representation of blocks

--- adoc2json/adoc2json.rb
+++ adoc2json/adoc2json.rb
@@ -6,14 +6,18 @@ require 'json'
 # Convert all instance variables of an object to a hash;
 # useful for the later conversion to JSON
 def object_to_hash(o)
-  o.instance_variables.each_with_object({}) {|var, hash| hash[var.to_s.delete("@")] = o.instance_variable_get(var)}
+  h = o.instance_variables.each_with_object({}) {|var, hash| hash[var.to_s.delete("@")] = o.instance_variable_get(var)}
+
+  # In the hash, add the converted DocBook representation of the object
+  h["docbook"] = o.convert
+  return h
 end
 
 # Get the path to the input file from ARGV
 in_file = ARGV[0]
 
-# Let asciidoctor parse the input file
-doc = Asciidoctor.load_file in_file
+# Let asciidoctor parse the input file, using DocBook as the target converter
+doc = Asciidoctor.load_file in_file, attributes: {'backend' => 'docbook'}
 parsed = doc.parse
 
 # Convert the parsed object to a hash
@@ -21,7 +25,7 @@ hash = object_to_hash parsed
 
 # Perform the same conversion with the "blocks" array;
 # otherwise, we'd get just the most basic information about paragraphs
-hash["blocks"] = hash["blocks"].map {|b| object_to_hash b}
+hash["blocks"].map! {|b| object_to_hash b}
 
 # Output a pretty JSON
 puts JSON.pretty_generate hash
